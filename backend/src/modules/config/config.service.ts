da1cff70afc150db93d7c74d93f64b23a7df09a4
Chidubem <nwabuisicelestine@gmail.com>
2022-08-04 13:11:18 +0100

    modified config on backend to allow successful build

--- backend/src/modules/config/config.service.ts
+++ backend/src/modules/config/config.service.ts
@@ -42,8 +42,8 @@ export class ConfigService {
       LOGGLY_SUBDOMAIN: Joi.string(),
       LOGGLY_TOKEN: Joi.string(),
       TYPEORM_ENTITIES: Joi.string().required(),
-      //TYPEORM_USERNAME: Joi.string().required(),
-      //TYPEORM_PASSWORD: Joi.string().required(),
+      TYPEORM_USERNAME: Joi.string().required(),
+      TYPEORM_PASSWORD: Joi.string().required(),
       TYPEORM_DATABASE: Joi.string().required(),
       TYPEORM_HOST: Joi.string().required(),
       TYPEORM_PORT: Joi.number()
